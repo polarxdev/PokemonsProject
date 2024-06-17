5643faa70b6f5b59ba646cc04afc72264459ef08
polarxdev <artempolityko200414@mail.ru>
2024-06-17 11:32:41 +0300

    Added pop-up message

--- app/src/main/java/com/example/pokemonsproject/DetailsActivity.kt
+++ app/src/main/java/com/example/pokemonsproject/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.example.pokemonsproject
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -59,6 +60,7 @@ class DetailsActivity : AppCompatActivity() {
             }
             else{
                 Log.d("pokeData", "pokeData is null")
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
             }
         }
     }
